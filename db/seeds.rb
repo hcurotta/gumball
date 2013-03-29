d20172dc246766e50b9b8c65c60d7d3b7d330247
Harry Curotta <hcurotta@gmail.com>
2013-03-30 09:55:38 +1100

    wrote data migration for existing store

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Migrating Alice's Store to V2
+
+s = Store.create({:name => "De La Vela", :owner_email => "alicehboyd@gmail.com", :fb_page_id => '306047546189127' })
+
+Product.all.each do |product|
+  product.store_id = s.id
+  product.save
+end
